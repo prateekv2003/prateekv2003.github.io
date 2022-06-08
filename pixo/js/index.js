a807604ae0d1dcdd3e71be4bd2aec3eee9ca93e3
Prateek Vishwakarma <89628118+prateekv2003@users.noreply.github.com>
2022-06-08 13:30:46 +0530

    Delete pixo directory

--- pixo/js/index.js
+++
@@ -1,6 +0,0 @@
-const parallax = document.getElementsByClassName("parallax-effect");
-
-window.addEventListener("scroll", function(){
-    let offset = window.scrollY;
-    parallax.item(0).style.backgroundPositionY = offset * -0.2 + "px";
-})
